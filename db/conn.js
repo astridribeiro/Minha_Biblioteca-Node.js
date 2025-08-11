17edcf287a12568c13634804d1b05b2221f6f60b
Astrid Rodrigues Ribeiro <astridrodriguesribeiro@gmail.com>
2025-08-11 12:41:28 -0300

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,12 +1,10 @@
-// OUTRA FORMA DE CONECTAR COM O SQL
-
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
     user: 'root',
-    password: 'astridRR1505.',
+    password: '************',
     database: 'nodemysql',
 });
 module.exports = pool;
